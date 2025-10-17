462bfaa3a40bc74ad428c27309c1681c71368b83
Ajay2122 <kapuriyaajay21@gmail.com>
2025-10-17 14:56:44 +0530

    17-10 logic

---
+++ 64.js
@@ -0,0 +1,20 @@
+// Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+// Output: 7
+// Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+// Example 2:
+
+// Input: grid = [[1,2,3],[4,5,6]]
+// Output: 12
+ 
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+
+    var q1 = (grid).toString().trim()
+    console.log(q1);
+    
+};
+const ans = minPathSum([[1,2,3],[4,5,6]])
